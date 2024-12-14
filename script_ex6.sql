18e40b486633fcdc5f84287c3dc8e602ece63cd7
bloodybear <sincarisebastian@gmail.com>
2024-12-14 02:05:49 +0200

    6

--- script_ex6.sql
+++ script_ex6.sql
@@ -1,50 +1,57 @@
 create or replace procedure profit_maxim(
     id_firma_1 in number,
-    id_firma_2 in number,
+    nume_prod in producator_camioane.nume%type,
     id_firma_3 in number
 )
 is
-
-
-    arr1 arr_nested := arr_nested();
-    type arr_indexed is table of number index by binary_integer;
-    arr2 arr_indexed;
-    arr3 arr_varray := arr_varray();
+    type arr_nested is table of number;
+        list_km_soferi arr_nested;
+    type arr_associative is table of number index by producator_camioane.nume%type;
+        list_nr_camioane arr_associative;
+    type arr_varray is varray(5) of date;
+        list_date_revizii arr_varray;
+    v_nr_prod_camioane number;
+    v_nume_prod producator_camioane.nume%type;
+    ex_key_not_found exception ;
 begin
 
     select t.nr_kilometri
-    bulk collect into arr1
+        bulk collect into  list_km_soferi
     from transporturi t
     join soferi s on s.id_sofer = t.id_sofer
-    join firma_de_transport f on f.id_firma = s.id_firma
-    where f.id_firma = id_firma_1
-    fetch first 10 rows only ;
+    where s.id_firma = id_firma_1;
 
-    select t.nr_kilometri
-    bulk collect into arr2
-    from transporturi t
-    join soferi s on s.id_sofer = t.id_sofer
-    join firma_de_transport f on f.id_firma = s.id_firma
-    where f.id_firma = id_firma_2
-    fetch first 10 rows only ;
+    select count(*)
+        into v_nr_prod_camioane
+    from producator_camioane;
 
-    select t.nr_kilometri
-    bulk collect into arr3
-    from transporturi t
-    join soferi s on s.id_sofer = t.id_sofer
-    join firma_de_transport f on f.id_firma = s.id_firma
-    where f.id_firma = id_firma_3
-    fetch first 10 rows only ;
+    for i in 1..v_nr_prod_camioane loop
+        select nume
+            into v_nume_prod
+        from producator_camioane
+            where id_prod_camion = i;
 
-    for i in arr1.first..arr1.last loop
-        dbms_output.PUT_LINE(arr1(i));
+        select count(*)
+            into list_nr_camioane(upper(v_nume_prod))
+        from camioane
+            where id_prod_camion = i and  id_firma = id_firma_1;
         end loop;
 
 
+    for i in list_km_soferi.first..list_km_soferi.last loop
+        dbms_output.PUT_LINE(list_km_soferi(i));
+        end loop;
+
+    if list_nr_camioane.exists(nume_prod) then
+        dbms_output.PUT_LINE(list_nr_camioane(nume_prod));
+    else
+        raise ex_key_not_found;
+    end if;
+
+exception
+    when ex_key_not_found then
+        dbms_output.PUT_LINE('Numele producatorului nu este gasit');
+    when others then
+        dbms_output.PUT_LINE('Eroare necunoscuta');
 end;
 /
-
-
--- type arr_indexed is table of number index by binary_integer;
-create or replace type arr_varray is varray(10) of number;
-create or replace type arr_nested is table of number;
