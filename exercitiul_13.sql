1a86cba1968ffd08bf65038e6f7558332861f486
bloodybear <sincarisebastian@gmail.com>
2025-01-31 15:03:30 +0200

    update

--- exercitiul_13.sql
+++ exercitiul_13.sql
@@ -1,4 +1,18 @@
+create or replace type sofer_type as object (
+    id_sofer number,
+    id_firma number,
+    nume varchar2(40),
+    data_angajare date,
+    nr_transporturi number
+);
+/
 create or replace type arr_soferi as table of sofer_type;
+/
+create or replace type sofer_camion as object (
+    id_sofer number,
+    id_camion number
+);
+/
 
 create or replace package rezervare_transport as
     type sofer_type as object (
